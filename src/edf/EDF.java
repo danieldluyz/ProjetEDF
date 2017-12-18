9c9726844136d0eb63392180a67988a347ea4931
danieldluyz <danieldluyz@172.20.78.243>
2017-12-18 14:49:05 +0100

    Basse

--- src/edf/EDF.java
+++ src/edf/EDF.java
@@ -4,7 +4,30 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.Solver;
+import org.chocosolver.solver.variables.IntVar;
+
 public class EDF {
+	
+	private static final int NB_EQUIPES = 2;
+	private static final int NB_FORMATEURS = 3;
+	private static final int NB_SALLES = 2;
+	
+	public EDF() {
+		Model model = new Model();
+		Solver solver = model.getSolver();
+		
+		IntVar equipe1 =  model.intVar(1,NB_FORMATEURS);
+		IntVar equipe2 = model.intVar(1,NB_FORMATEURS);
+		
+		IntVar formateur1 = model.intVar(1,NB_EQUIPES);
+		IntVar formateur2 = model.intVar(1,NB_EQUIPES);
+		IntVar formateur3 = model.intVar(1,NB_EQUIPES);
+		
+		IntVar salle1 = model.intVar(1,NB_SALLES);
+		IntVar salle2 = model.intVar(1,NB_SALLES);
+	}
 	
 	public static void main(String[] args) {
 		File file = new File("./data/DisposEquipes.csv");
